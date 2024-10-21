e8fa66297a168939c837fbf0c19c4b6d27e74bc8
wcw <344078971@qq.com>
2024-10-21 14:03:45 +0800

    fix: remove dom zooming, it will affect cv printing

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import { createSignal, onMount, type Component } from 'solid-js';
+import { type Component } from 'solid-js';
 
 import { DefaultLayout } from '@/components/layouts/default';
 
@@ -11,7 +11,6 @@ const Home: Component = () => {
   const { t } = context;
   const theme = () => (context.isDark ? t('global.light_mode') : t('global.dark_mode'));
 
-  const [zoom, setZoom] = createSignal(1)
   const switchTheme = () => {
     context.setDark(!context.isDark)
   }
@@ -32,57 +31,11 @@ const Home: Component = () => {
     }
   }
 
-  onMount(resize)
-  useEventListener(window, 'resize', resize)
-  useEventListener(window, 'onbeforeprint', beforePrint)
-  useEventListener(window, 'onafterprint', afterPrint)
-
-  if (window.matchMedia) {
-    const mediaQueryList = window.matchMedia('print');
-    mediaQueryList.addListener(function (mql) {
-      if (mql.matches) {
-        beforePrint();
-      } else {
-        afterPrint();
-      }
-    });
-  }
-
-  function beforePrint() {
-    setZoom(1)
-  }
-
-  function afterPrint() {
-    resize()
-  }
-
-  function resize() {
-    const a4 = [
-      [595, 842],
-      [1240, 1754],
-      [2480, 3508]
-    ]
-    const { innerWidth, innerHeight } = window
-    const orientation = innerWidth > innerHeight ? 'portrait' : 'landscape'
-
-    for (const size of a4) {
-      const [w, h] = size
-
-      if (orientation === 'portrait' && innerHeight < h) {
-        setZoom(innerHeight / h - 0.05)
-        break;
-      } else if (orientation === 'landscape' && innerWidth < w) {
-        setZoom(innerWidth / w - 0.05)
-        break
-      }
-    }
-  }
-
   return (
-    <div class="center relative min-w-100vw min-h-100vh gap-2" style={`zoom: ${zoom()};`}>
+    <div class="center  relative min-w-100vw min-h-100vh gap-2">
       <DefaultLayout></DefaultLayout>
 
-      <div id="buttons" class="absolute right-0 translate-x-100% h-full flex flex-col gap-4">
+      <div id="buttons" class="fixed right-0 top-0 translate-x--100% translate-y-25% h-full flex flex-col gap-4">
         <CVButton onClick={switchLang}>{t('global.lang')}</CVButton>
         <CVButton onClick={switchTheme}>{theme()}</CVButton>
         <CVButton onClick={printCV}>{t('global.print')}</CVButton>
